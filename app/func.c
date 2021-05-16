49a7ed56fce7aef23fdedb7cfa97279b10fe8c07
VM User <bryfixe@mail.ru>
2021-05-17 01:41:57 +0300

    commit

---
+++ app/func.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include "func.h"
+
+solution Func(double a, double b, double c) {
+    solution res;
+    res.count = 0;
+
+    if(a == 0){
+        if(b == 0){
+            res.count = 0;
+            return res;
+        }else{
+        res.x1 = (-c)/b;
+        res.count = 1;
+        return res;
+        }
+    }
+
+    double D = pow(b, 2) - 4*a*c;
+
+    if(D < 0.0) {
+        res.count = 0;
+
+    }
+    if(D == 0.0) {
+        res.x1 = -b/(2*a);
+         res.count = 1;
+        
+    }
+    if(D > 0.0) {
+        res.x1 = (-b - sqrt(D)) / (2*a);
+        res.x2 = (-b + sqrt(D)) / (2*a);
+         res.count = 2;
+    }
+
+    return res;
+}
---
+++ app/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "myfunc.h"
+#include "func.c"
+
+
+int main()
+{
+    printf("Hello World!\n");
+    return myfunc(2);
+}
+
+
+
+
